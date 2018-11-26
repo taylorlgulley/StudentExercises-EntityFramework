d6f1da5e865cb3d7c05ca255eaae51fd0992bc2d
Meg Ducharme <ducharme.meg@gmail.com>
2018-11-26 13:47:04 -0600

    live code ready

--- StudentExercisesWebApp/Models/ViewModels/CreateStudentViewModel.cs
+++ StudentExercisesWebApp/Models/ViewModels/CreateStudentViewModel.cs
@@ -12,6 +12,7 @@ namespace StudentExercisesWebApp.Models.ViewModels
         public Student Student { get; set; }
         public List<SelectListItem> AvailableExercises { get; private set; }
         public List<int> SelectedExercises { get; set; }
+        public List<SelectListItem> Cohorts { get; set; }
 
         public CreateStudentViewModel() { }
         public CreateStudentViewModel(ApplicationDbContext ctx)
@@ -21,6 +22,12 @@ namespace StudentExercisesWebApp.Models.ViewModels
             {
                 Text = li.Name,
                 Value = li.ExerciseId.ToString()
+            }).ToList();
+
+            Cohorts = ctx.Cohorts.Select(li => new SelectListItem()
+            {
+                Text = li.Name,
+                Value = li.CohortId.ToString()
             }).ToList();
         }
     }
